3f5d7b902221d86fe4c49e8a85c3212b2a15f397
Soham Jain <36758674+sohamsjain@users.noreply.github.com>
2022-04-08 16:27:00 +0530

    replaceing package pynse with nsepy updating checkAdjustedClose.py to use nsepy instead of pynse updating requirements.txt

--- checkAdjustedClose.py
+++ checkAdjustedClose.py
@@ -1,33 +1,19 @@
-from datetime import datetime, timedelta
-from typing import List
+from datetime import date, time
 
 import backtrader as bt
-import pandas_market_calendars
-from pynse import Nse, IndexSymbol
+from nsepy import get_history
 
-pmcnse = pandas_market_calendars.get_calendar("NSE")
+from tradingschedule import lastclosingtime
 
-schedule = \
-    pmcnse.schedule(datetime.now().date() - timedelta(days=10), datetime.now().date() + timedelta(days=10),
-                    tz="Asia/Kolkata")[
-        "market_close"].to_list()
+nsepyclose = get_history(symbol="NIFTY 50",
+                         start=date(2022, 4, 6),
+                         end=date(2022, 4, 8),
+                         index=True)["Close"].values[-1]
 
-schedule = [t.to_pydatetime().replace(tzinfo=None) for t in schedule]
-
-now = datetime.now()
-
-closes = [dt for dt in schedule if dt < now]
-pastcloses: List[datetime] = [dt for dt in schedule if dt < now]
-futurecloses = [dt for dt in schedule if dt > now]
-
-nse = Nse()
-
-pynseclose = nse.get_indices(IndexSymbol.Nifty50)["last"][0]
 adjustedClose = False
-fromdate = pastcloses[-1].date()
-todate = pastcloses[-1].date()
-sessionstart = datetime.now().time().replace(hour=9, minute=15, second=0, microsecond=0)
-sessionend = datetime.now().time().replace(hour=15, minute=30, second=0, microsecond=0)
+fromdate = todate = lastclosingtime.date()
+sessionstart = time(hour=9, minute=15)
+sessionend = time(hour=15, minute=30)
 
 
 class TestSt2(bt.Strategy):
@@ -36,8 +22,8 @@ class TestSt2(bt.Strategy):
         self.cerebro.runstop()
 
     def stop(self):
-        global adjustedClose, pynseclose
-        if self.datas[0].close[0] == pynseclose:
+        global adjustedClose, nsepyclose
+        if self.datas[0].close[0] == nsepyclose:
             adjustedClose = True
 
 
