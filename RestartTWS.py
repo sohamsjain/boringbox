03795b6adaadff992456f9d3a8a612b5d9e82fd2
Soham Jain <36758674+sohamsjain@users.noreply.github.com>
2022-05-09 18:22:19 +0530

    simplifying RestartTWS.py

--- RestartTWS.py
+++ RestartTWS.py
@@ -1,19 +1,5 @@
-from backtrader.stores import IBStore
-
 from mytelegram.raven import Raven
 
 raven = Raven()
 
-
-def listener(message):
-    global store, raven
-    print(message)
-    if message.errorCode == 502:
-        raven.send_all_clients("Restart TWS on EC2")
-
-
-store = IBStore(port=7497)
-store.conn.registerAll(listener)
-store.start()
-store.stop()
-raven.stop()
+raven.send_all_clients("Restart TWS on EC2")
